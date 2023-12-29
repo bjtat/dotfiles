ca7cceb8840b21fed62f54abaed9e0581e38230b
Brandon Tat <brandontat6@gmail.com>
2023-12-28 20:23:38 -0800

    change lazy-lock and disable plugins

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,23 @@
+local disable_keys = function()
+  return {}
+end
+
+return {
+  -- stylua: ignore
+  { "akinsho/bufferline.nvim", enabled = false, keys = disable_keys, },
+  { "echasnovski/mini.ai", enabled = false, keys = disable_keys },
+  { "echasnovski/mini.bufremove", enabled = false, keys = disable_keys },
+  { "echasnovski/mini.indentscope", enabled = false, keys = disable_keys },
+  { "echasnovski/mini.pairs", enabled = false, keys = disable_keys },
+  { "echasnovski/mini.surround", enabled = false, keys = disable_keys },
+  { "folke/flash.nvim", enabled = false, keys = disable_keys },
+  { "folke/neoconf.nvim", enabled = false, keys = disable_keys },
+  { "folke/noice.nvim", enabled = false, keys = disable_keys },
+  { "folke/todo-comments.nvim", enabled = false, keys = disable_keys },
+  { "folke/trouble.nvim", enabled = false, keys = disable_keys },
+  { "nvim-pack/nvim-spectre", enabled = false, keys = disable_keys },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false, keys = disable_keys },
+  { "rafamadriz/friendly-snippets", enabled = false, keys = disable_keys },
+  { "rcarriga/nvim-notify", enabled = false, keys = disable_keys },
+  { "windwp/nvim-ts-autotag", enabled = false, keys = disable_keys },
+}
