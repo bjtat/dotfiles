7394cbe14029e407cd2c1a5de79274a984ee8309
Brandon Tat <brandontat6@gmail.com>
2024-02-03 17:00:46 -0800

    Add ideavimrc and updating lazyvim plugins

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,13 @@
 -- Add any additional options here
 
 vim.opt.scrolloff = 8 -- Lines of context
+
+-- sainhe/gruxbox-material global Vim settings
+vim.g.gruvbox_material_disable_italic_comment = 1
+
+-- original gruvbox settings
+vim.g.gruvbox_material_enable_bold = 0
+vim.g.gruvbox_material_background = "medium"
+vim.g.gruvbox_material_foreground = "original"
+
+vim.o.statuscolumn = "%s %l %r"
