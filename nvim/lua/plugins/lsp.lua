69b9c9000eb17894af99c739e9cee78eff88c53b
Brandon Tat <brandontat6@gmail.com>
2025-04-21 13:49:08 -0700

    ruby support for lazyvim + zed

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruby_lsp = {
+        mason = false,
+        -- don't run with bundle, https://shopify.github.io/ruby-lsp/composed-bundle.html
+        cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+      },
+      rubocop = {
+        mason = false,
+        -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
+        -- NOTE: if rubocop fails, check running processes and see if there's an old one
+        cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        --   root_dir = require("lspconfig").util.root_pattern("Gemfile", ".git", "."),
+      },
+    },
+  },
+}
