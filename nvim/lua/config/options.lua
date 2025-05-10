55926fc818fd35228ea7621c70990085d289e846
Brandon Tat <brandontat6@gmail.com>
2025-05-09 23:54:07 -0700

    updates

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,4 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazyvim_picker = "snacks"
 vim.g.snacks_animate = false
+
+vim.g.lazyvim_ruby_lsp = "ruby_lsp"
+vim.g.lazyvim_ruby_formatter = "rubocop"
---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+  { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.files", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+}
